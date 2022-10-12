94b78ce07846fcadfa2dc5910e8257a426e3c94e
Jonathan Dickinson <uhr717@gmail.com>
2022-10-11 20:51:11 -0400

    dataset branch

--- window.py
+++ window.py
@@ -10,7 +10,7 @@ class Window(Frame):
     def init_window(self):
         self.master.title("GUI")
         self.pack(fill=BOTH, expand=1)
-#sdfasdfa
+        
         menu = Menu(self.master)
         self.master.config(menu=menu)
 
