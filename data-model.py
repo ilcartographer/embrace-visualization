5f1e127130ba73acf1f2ff42e0ad44dd0be36950
Joseph-Lyvers <115122454+Joseph-Lyvers@users.noreply.github.com>
2022-10-15 18:34:50 -0400

    Made basic data model class, may need additional methods/variables

---
+++ data-model.py
@@ -0,0 +1,6 @@
+import pandas as pd # needs pandas installed
+class datamodel:
+    def __init__(self,csvurl): #csvurl referring to summary
+        self.data = pd.read_csv(csvurl)
+    def timeYformat(self, time, Y): # return dataframe with only the specified time variable and Y variable
+        return self.data.iloc[:,[time,Y]]
