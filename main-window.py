e47403ccbb4f950f6eddda532e0afe1646dc7e2a
ilcartographer <ilcartographer90@gmail.com>
2022-10-22 22:45:39 -0400

    Bug fix for feature selector

--- main-window.py
+++ main-window.py
@@ -133,7 +133,8 @@ class GraphPage(Frame):
         self.dm.setnewurl(filename)
         self.disp.set(self.dm)
 
-        #self.controller.plot()  # Creates the graphs when the "OK" button is clicked in Load Data
+        # self.controller.plot()  # Creates the graphs when the "OK" button is clicked in Load Data
+
 
 class LoadDataForm:
     def __init__(self, top, graph_page, main_window):
@@ -190,9 +191,9 @@ class LoadDataForm:
                              command=lambda value=string: callback(value))
 
     def submit(self, data):
-        #self.submit_load_action(data.get())
-        #change path to work with your computer
-        self.submit_load_action("C:/Users/jdick/Downloads/embrace-visualization/Dataset/20200120/312/summary.csv")
+        # self.submit_load_action(data.get())
+        # change path to work with your computer
+        self.submit_load_action("Dataset/20200120/312/summary.csv")
         self.show_time_series_builder()
         self.master.destroy()
 
@@ -200,13 +201,14 @@ class LoadDataForm:
         time_series_window = Toplevel(height='1000', width='1000')
         TimeSeriesBuilder(time_series_window, self.get_column_names(), self.main_window)
 
+
 class TimeSeriesBuilder:
     def __init__(self, window, column_names, main_window):
         self.master = window
         self.column_names = column_names
         self.main_window = main_window
         self.add_widgets()
-    
+
     def add_widgets(self):
         first_frame_vertical = Frame(self.master)
         available_label = Label(first_frame_vertical, text="Available Series", font=SMALL_FONT)
@@ -220,21 +222,23 @@ class TimeSeriesBuilder:
             lb_available.insert(index, series_name)
         lb_available.pack(side='left', anchor='w')
         second_frame_horizontal = Frame(second_frame_vertical)
-        right_arrow_photo = PhotoImage(file = 'right_arrow.png')
+        right_arrow_photo = PhotoImage(file='right_arrow.png')
 
         # store extra reference to photo so gc doesn't clear
         gc_right_label = Label(image=right_arrow_photo)
         gc_right_label.image = right_arrow_photo
 
-        right_arrow_button = Button(second_frame_horizontal, height = 20, width = 40, image = right_arrow_photo, command=lambda: self.move_selections(lb_available, lb_selected))
+        right_arrow_button = Button(second_frame_horizontal, height=20, width=40, image=right_arrow_photo,
+                                    command=lambda: self.move_selections(lb_available, lb_selected))
         right_arrow_button.pack(side='top', anchor='n')
-        left_arrow_photo = PhotoImage(file = 'left_arrow.png')
+        left_arrow_photo = PhotoImage(file='left_arrow.png')
 
         # store extra reference to photo so gc doesn't clear
         gc_left_label = Label(image=left_arrow_photo)
         gc_left_label.image = left_arrow_photo
 
-        left_arrow_button = Button(second_frame_horizontal, height = 20, width = 40, image = left_arrow_photo, command=lambda: self.move_selections(lb_selected, lb_available))
+        left_arrow_button = Button(second_frame_horizontal, height=20, width=40, image=left_arrow_photo,
+                                   command=lambda: self.move_selections(lb_selected, lb_available))
         left_arrow_button.pack(side='bottom', anchor='s')
         second_frame_horizontal.pack(side='left')
         lb_selected = Listbox(second_frame_vertical, selectmode='multiple')
@@ -246,32 +250,16 @@ class TimeSeriesBuilder:
         third_frame_vertical.pack(fill='x')
 
     def move_selections(self, lb_origin, lb_destination):
-        #selected_option_indices = lb_origin.curselection()
-        #print(selected_option_indices)
         for option_index in lb_origin.curselection():
-            print(option_index)
-            print(lb_origin.get(option_index))
             lb_destination.insert(option_index, lb_origin.get(option_index))
-            #lb_origin.delete(option_index)
 
-        for option_index in lb_origin.curselection():
+        for option_index in reversed(lb_origin.curselection()):
             lb_origin.delete(option_index)
-        '''
-        selected_option_indices = lb_origin.curselection()
-        cur_index = 0
-        while len(selected_option_indices) > 0:
-            index = selected_option_indices[cur_index]
-            lb_destination.insert(index, lb_origin.get(index))
-            lb_origin.delete(index)
-            cur_index +=1
-        '''
-
-
-
 
     def finish(self):
         self.main_window.plot()  # Creates the graphs when the "OK" button is clicked in Load Data
         self.master.destroy()
+
 
 app = MainWindow()
 app.mainloop()
