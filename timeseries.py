a5999e97ea595db168a5a04cc40101233e65b3ea
ilcartographer <ilcartographer90@gmail.com>
2022-11-07 22:04:44 -0500

    Fixing issues with clearing previous graphs

--- timeseries.py
+++ timeseries.py
@@ -23,27 +23,6 @@ class TimeSeries:
                 "canvas": AggregatedDataSet(self.parent, self.dm.getdatasetforfeature(time, feature)).render()
             }])
 
-        # points_x1 = time
-        # points_y1 = ind
-        # # the figure that will contain the plot
-        # fig_1 = Figure(figsize=(10, 2.5), dpi=100)
-        # # adding the subplot (I don't know exactly what a subplot does, but this defines the graph)
-        # plot1 = fig_1.add_subplot(111)
-        # # plotting graph 1
-        # plot1.plot(points_x1, points_y1)
-        # # creating the Tkinter canvas which houses the graphs
-        # canvas = FigureCanvasTkAgg(fig_1, master=self.main_window)
-        # # appending canvas info to the canvases list so they can be destroyed later
-        # self.canvases.append([
-        #     {
-        #         "label": series_name,
-        #         "canvas": canvas
-        #     }])
-        # plot_label = Label(self.main_window, text=series_name, font=LARGE_FONT)
-        # plot_label.pack()
-        # canvas.draw()
-        # canvas.get_tk_widget().pack()
-
     def plot_selected_group(self, selected_time_series_names):
         self.remove_all_plots()
 
@@ -53,6 +32,6 @@ class TimeSeries:
     def remove_all_plots(self):
         # TODO get this method to work correctly. It is not deleting the canvases currently
         for plot in self.canvases:
-            plot["canvas"].get_tk_widget().delete('all')
+            plot[0]["canvas"].get_tk_widget().destroy()
 
         self.canvases.clear()
